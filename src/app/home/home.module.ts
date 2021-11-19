daa9f30fb31a7af82883cfcd129bd2a446349e91
delbertina <delbertina96@gmail.com>
2021-11-18 23:55:51 -0600

    Styling fixes for home page on mobile

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import {MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class HomeModule { }
