526a77894f49c4f1c0d45be52b9fdceab4ec32c7
delbertina <delbertina96@gmail.com>
2022-01-22 16:07:16 -0600

    Update rules page and discord widget

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -12,8 +12,18 @@ export class HomeComponent implements OnInit {
   currentEvents: CardData[] = events["current-events"];
   pastEvents: CardData[] = events["past-events"];
 
+  discordURL = "https://bit.ly/MagnaRisaDiscord";
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public openNewTab(url: string): void {
+    window.open(url, '_blank');
+  }
+
+  public randomOnline(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
   }
 }
